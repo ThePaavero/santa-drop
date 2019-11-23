0f8e4f3dda22218d0fc73b571eda889f3c550c54
Pekka Siiriäinen <pekka.siiriainen@mirumagency.com>
2019-11-23 22:55:42 +0200

    Boilerplate.

--- main.js
+++ main.js
@@ -10,11 +10,40 @@ const images = [
   'skySlice',
 ]
 
+const state = {
+  world: {
+    x: 0,
+  },
+  player: {
+    x: 0,
+    y: 0,
+  },
+  presentsInAir: [],
+  houses: [],
+}
+
+const dropPresent = () => {
+  state.presentsInAir.push({
+    x: state.player.x,
+    y: state.player.y,
+    velocities: {
+      x: 0,
+      y: 3,
+    },
+  })
+}
+
+const updateState = () => {
+  state.presentsInAir.forEach(present => {
+    present.y += present.velocities.y
+  })
+}
+
 playground({
 
-  width: 640,
-  height: 480,
-  scale: 2,
+  width: 1024,
+  height: 768,
+  scale: 1,
 
   create: function() {
     images.forEach(imgSlug => {
@@ -29,9 +58,17 @@ playground({
   },
 
   step: function(dt) {
+    updateState()
   },
 
   render: function() {
+    this.layer.clear("#000")
+
+    // Draw the sky.
+    this.layer.drawImage(this.images.skySlice, 0, 0, this.width, this.height)
+
+    // Draw presents in the air.
+    // @todo
   },
 
   keydown: function(data) {
@@ -44,6 +81,7 @@ playground({
   },
 
   mouseup: function(data) {
+    dropPresent()
   },
 
   mousemove: function(data) {
